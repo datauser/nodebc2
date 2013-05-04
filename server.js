d2d4bea79bda070f4445e513b2121ac689c00766
Girish <varsha@Varshas-MacBook-Air.local>
2013-05-04 15:09:15 -0700

    express

--- server.js
+++ server.js
@@ -1,23 +1,68 @@
-// 1. nodejs.org
-//    http://howtonode.org/ 
-// 2. http://net.tutsplus.com/tutorials/javascript-ajax/learning-serverside-javascript-with-Node-js/
-//    http://net.tutsplus.com/tutorials/javascript-ajax/creating-a-to-do-list-app-with-node-js-and-geddy/
+#!/bin/env node
+//  OpenShift sample Node application
+// 
+var express = require('express');
+var fs      = require('fs');
 
-var http = require('http');
+//  Local cache for static content [fixed and loaded at startup]
+var zcache = { 'index.html': '' };
+zcache['index.html'] = fs.readFileSync('./index.html'); //  Cache index.html
 
-//Get the environment variables we need.
-var ipaddr  = process.env.AZURE_INTERNAL_IP || "127.0.0.1";
-var port    = process.env.PORT || 8080;
+// Create "express" server.
+var app  = express.createServer();
 
-// The anonymous function that is passed into http.createServer will be 
-// called whenever a request comes in to the server.
-http.createServer(function (req, res) {
-  // http://nodejs.org/api/http.html
-  // http connection status code
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  // send response and close connection
-  res.write("Hello World\n");
-  res.end('oh well' + ipaddr, + '\n');
-}).listen(port, ipaddr);
-console.log('nodebc2 at azurewebsites' );
+
+/*  =====================================================================  */
+/*  Setup route handlers.  */
+/*  =====================================================================  */
+
+// Handler for GET /health
+app.get('/health', function(req, res){
+    res.send('1');
+});
+
+// Handler for GET /asciimo
+app.get('/asciimo', function(req, res){
+    var link="https://a248.e.akamai.net/assets.github.com/img/d84f00f173afcf3bc81b4fad855e39838b23d8ff/687474703a2f2f696d6775722e636f6d2f6b6d626a422e706e67";
+    res.send("<html><body><img src='" + link + "'></body></html>");
+});
+
+// Handler for GET /
+app.get('/', function(req, res){
+    res.send(zcache['index.html'], {'Content-Type': 'text/html'});
+});
+
+
+//  Get the environment variables we need.
+var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || 'localhost';
+var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+
+if (typeof ipaddr === "undefined") {
+   console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
+}
+
+//  terminator === the termination handler.
+function terminator(sig) {
+   if (typeof sig === "string") {
+      console.log('%s: Received %s - terminating Node server ...',
+                  Date(Date.now()), sig);
+      process.exit(1);
+   }
+   console.log('%s: Node server stopped.', Date(Date.now()) );
+}
+
+//  Process on exit and signals.
+process.on('exit', function() { terminator(); });
+
+['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
+ 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'
+].forEach(function(element, index, array) {
+    process.on(element, function() { terminator(element); });
+});
+
+//  And start the app on that interface (and port).
+app.listen(port, ipaddr, function() {
+   console.log('%s: Node server started on %s:%d ...', Date(Date.now() ),
+               ipaddr, port);
+});
 
