deffbac9c8df440d28dbfe92a9e9685c029a58c8
Girish <varsha@Varshas-MacBook-Air.local>
2013-05-05 13:15:35 -0700

    module fs

--- server.js
+++ server.js
@@ -1,68 +1,21 @@
-#!/bin/env node
-//  OpenShift sample Node application
-// 
-var express = require('express');
-var fs      = require('fs');
+// Shelley Powers. Learning Node (Kindle Locations 602-608). O'Reilly Media. 
+// load http module 
+var http = require('http'); 
+var fs = require('fs'); 
 
-//  Local cache for static content [fixed and loaded at startup]
-var zcache = { 'index.html': '' };
-zcache['index.html'] = fs.readFileSync('./index.html'); //  Cache index.html
-
-// Create "express" server.
-var app  = express.createServer();
-
-
-/*  =====================================================================  */
-/*  Setup route handlers.  */
-/*  =====================================================================  */
-
-// Handler for GET /health
-app.get('/health', function(req, res){
-    res.send('1');
+// create http server 
+http.createServer( function (req, res) {
+	// open and read in server.js 
+	fs.readFile('server.js', 'utf8', function( err, data) {
+		res.writeHead( 200, {'Content-Type': 'text/ plain'});
+		if (err)
+			res.write('Could not find or open file for reading\ n');
+		else // if no error, write JS file to client 
+			res.write( data);
+		res.end();
+	 });
+}).listen( 8124, function() {
+	 console.log('bound to port 8124');
 });
-
-// Handler for GET /asciimo
-app.get('/asciimo', function(req, res){
-    var link="https://a248.e.akamai.net/assets.github.com/img/d84f00f173afcf3bc81b4fad855e39838b23d8ff/687474703a2f2f696d6775722e636f6d2f6b6d626a422e706e67";
-    res.send("<html><body><img src='" + link + "'></body></html>");
-});
-
-// Handler for GET /
-app.get('/', function(req, res){
-    res.send(zcache['index.html'], {'Content-Type': 'text/html'});
-});
-
-
-//  Get the environment variables we need.
-var ipaddr  = process.env.OPENSHIFT_INTERNAL_IP || 'localhost';
-var port    = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
-
-if (typeof ipaddr === "undefined") {
-   console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
-}
-
-//  terminator === the termination handler.
-function terminator(sig) {
-   if (typeof sig === "string") {
-      console.log('%s: Received %s - terminating Node server ...',
-                  Date(Date.now()), sig);
-      process.exit(1);
-   }
-   console.log('%s: Node server stopped.', Date(Date.now()) );
-}
-
-//  Process on exit and signals.
-process.on('exit', function() { terminator(); });
-
-['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
- 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'
-].forEach(function(element, index, array) {
-    process.on(element, function() { terminator(element); });
-});
-
-//  And start the app on that interface (and port).
-app.listen(port, ipaddr, function() {
-   console.log('%s: Node server started on %s:%d ...', Date(Date.now() ),
-               ipaddr, port);
-});
+console.log('Server running on 8124/');
 
