be24b52346eefc8884a2679df352d31e947b62e1
Girish <varsha@Varshas-MacBook-Air.local>
2013-05-05 13:21:39 -0700

    module fs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ http.createServer( function (req, res) {
 	fs.readFile('server.js', 'utf8', function( err, data) {
 		res.writeHead( 200, {'Content-Type': 'text/ plain'});
 		if (err)
-			res.write('Could not find or open file for reading\ n');
+			res.write('Could not find or open file for reading\n');
 		else // if no error, write JS file to client 
 			res.write( data);
 		res.end();
@@ -17,5 +17,5 @@ http.createServer( function (req, res) {
 }).listen( 8124, function() {
 	 console.log('bound to port 8124');
 });
-console.log('Server running on 8124/');
+console.log('Server running on 8124');
 
